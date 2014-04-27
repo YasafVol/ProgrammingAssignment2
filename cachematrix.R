ea4e215525f1e2dfb920963a3104af2b6b77c4f4
Yasaf Volinsky <yasaf.vol@gmail.com>
2014-04-27 23:57:08 +0300

    the solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##creates a "special matrix" and a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        value <- NULL
+        set <- function(y) {
+                x <<- y
+                value <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(solve) value <<- solve
+        getInverse <- function() value
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Bizzaro (Inverse) matrix. If already there, returns cached version.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return inverse of x
+        value <- x$getInverse()
+        if(!is.null(value)) {
+                message("Getting data")
+                return(value)
+        }
+        data <- x$get()
+        value <- solve(data, ...)
+        x$setInverse(value)
+        value
 }
